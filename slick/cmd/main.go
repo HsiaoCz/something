ae542535d8d8f1e0615ace84145476e7c6da6e2a
HsiaoCz <984274788@qq.com>
2024-05-16 11:26:32 +0800

    some

--- slick/cmd/main.go
+++ slick/cmd/main.go
@@ -10,12 +10,25 @@ func main() {
 	app := slick.New()
 
 	app.GET("/user", HandleGetUser)
-
+	app.GET("/hello", HandleHello)
+	app.GET("/hello/:name", HandleHelloName)
 	app.Run(":9001")
 }
 
 func HandleGetUser(c *slick.Context) {
 	c.JSON(http.StatusOK, slick.H{
 		"message": "all is well",
+	})
+}
+
+func HandleHello(c *slick.Context) {
+	c.JSON(http.StatusOK, slick.H{
+		"message": "all is well",
+	})
+}
+
+func HandleHelloName(c *slick.Context) {
+	c.JSON(http.StatusOK, slick.H{
+		"message": c.Param("name"),
 	})
 }
